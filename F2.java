9fd7c073ee2ed22c316bc80093bbb1cc019943de
rkgowripriya <gowripriyasurya@gmail.com>
2016-07-21 09:27:39 +0530

    Create F2.java

---
+++ F2.java
@@ -0,0 +1,13 @@
+class F2{
+	
+		public static void main(String args[])
+		{
+		
+		
+		String s1=args[0];
+		StringBuffer buf=new StringBuffer(s1);
+		String s2=buf.reverse().toString();
+		System.out.println(s2);
+		
+		}
+	}
